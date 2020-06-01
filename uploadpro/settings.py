76376b901daca67aef247f94528c710c4f51bf34
Minasamy823 <minasamy823@gmail.com>
2020-06-01 19:30:05 +0300

    Add sqlite config.

--- uploadpro/settings.py
+++ uploadpro/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'uploadpro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'testingdb',
-        'USER': 'testingdb',
-        'PASSWORD': 'testingdb',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
